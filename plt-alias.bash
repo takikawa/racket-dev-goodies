a1f0463e9f86ce08b143696cdf8d64dc8332ea4e
Sam Tobin-Hochstadt <samth@racket-lang.org>
2013-07-13 12:41:27 -0400

    fix to be how sam likes it

--- plt-alias.bash
+++ plt-alias.bash
@@ -1,21 +1,5 @@
 # Put this in .bashrc
 
-# Alias the following binaries in your $PATH to this script:
-PATH_TO_PLT_BIN="plt-bin"
-
-alias drracket="$PATH_TO_PLT_BIN drracket"
-alias drscheme="$PATH_TO_PLT_BIN drscheme"
-alias gracket="$PATH_TO_PLT_BIN gracket"
-alias gracket-text="$PATH_TO_PLT_BIN gracket-text"
-alias mred="$PATH_TO_PLT_BIN mred"
-alias mred-text="$PATH_TO_PLT_BIN mred-text"
-alias mzscheme="$PATH_TO_PLT_BIN mzscheme"
-alias racket="$PATH_TO_PLT_BIN racket"
-alias raco="$PATH_TO_PLT_BIN raco"
-alias scribble="$PATH_TO_PLT_BIN scribble"
-alias slatex="$PATH_TO_PLT_BIN slatex"
-alias slideshow="$PATH_TO_PLT_BIN slideshow"
-
 function plt() {
 
    if [[ $# == 0 ]]; then
